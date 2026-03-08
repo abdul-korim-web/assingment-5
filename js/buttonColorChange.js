587fcb3cfffbca8be7fa00bd3d0a497fd65e0673
abdul-korim-web <moneyschorsing@gmail.com>
2026-03-08 22:42:26 +0600

    button Color Change  function added

---
+++ js/buttonColorChange.js
@@ -0,0 +1,11 @@
+const buttonColorChange = (id)=>{
+    console.log(id);
+    const allbtn = document.getElementById("Allbtn")
+    const openbtn = document.getElementById("Openbtn")
+    const closebtn = document.getElementById("Closedbtn")
+    const clickbtn = document.getElementById(id)
+    allbtn.classList.remove("active")
+    openbtn.classList.remove("active")
+    closebtn.classList.remove("active")
+    clickbtn.classList.add("active")
+}
